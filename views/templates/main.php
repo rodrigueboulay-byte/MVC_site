36a55701189b1888c3eebd4447b931c1a2d0932e
rodrigueboulay-byte <rodrigue.boulay@gmail.com>
2025-11-04 18:41:41 +0100

    Fix style Monitoring

--- views/templates/main.php
+++ views/templates/main.php
@@ -14,7 +14,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Emilie Forteroche</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=1">
 </head>
 
 <body>
